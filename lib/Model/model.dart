dbbb91d2e5f53f7d3ed704d94b24c32b994daab5
ibrahim elsokary <107218787+ibrahimElsokkary@users.noreply.github.com>
2023-07-22 02:16:12 +0300

    done with model

--- lib/Model/model.dart
+++ lib/Model/model.dart
@@ -1,1 +1,47 @@
 //TODO: add your model class here
+
+class StudentInfo {
+  final String schoolName;
+  final String schoolAddress;
+  final String numberOfStudents;
+  final String numberOfTeachers;
+
+  StudentInfo({
+    required this.schoolName,
+    required this.schoolAddress,
+    required this.numberOfStudents,
+    required this.numberOfTeachers,
+  });
+
+  factory StudentInfo.fromJson(Map<String, dynamic> json) {
+    return StudentInfo(
+      schoolName: json["School Name"],
+      schoolAddress: json["School Address"],
+      numberOfStudents: json["Number of Students"],
+      numberOfTeachers: json["Number of Teachers"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "School Name": schoolName,
+      "School Address": schoolAddress,
+      "Number of Students": numberOfStudents,
+      "Number of Teachers": numberOfTeachers,
+    };
+  }
+
+  StudentInfo copyWith(
+    String? schoolName,
+    String? schoolAddress,
+    String? numberOfStudents,
+    String? numberOfTeachers,
+  ) {
+    return StudentInfo(
+      schoolName: schoolName ?? this.schoolName,
+      schoolAddress: schoolAddress ?? this.schoolAddress,
+      numberOfStudents: numberOfStudents ?? this.numberOfStudents,
+      numberOfTeachers: numberOfTeachers ?? this.numberOfTeachers,
+    );
+  }
+}
