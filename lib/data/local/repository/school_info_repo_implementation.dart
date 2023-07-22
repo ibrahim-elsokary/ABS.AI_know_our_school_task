ad3fc68c55095f719e9def7a89f92d0c93f2b282
ibrahim elsokary <107218787+ibrahimElsokkary@users.noreply.github.com>
2023-07-22 06:20:11 +0300

    edit code to work with android

--- lib/data/local/repository/school_info_repo_implementation.dart
+++ lib/data/local/repository/school_info_repo_implementation.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/services.dart';
 import 'package:know_our_school/Model/model.dart';
 import 'package:know_our_school/constant.dart';
 import 'package:know_our_school/data/local/repository/school_info_repo.dart';
@@ -9,7 +10,7 @@ class SchoolInfoImple implements SchoolInfoRepo {
   @override
   Future<List<SchoolInfoModel>> getSchoolInfoList() async {
     try {
-      var jsonString = await File(AppConstant.jsonPath).readAsString();
+      var jsonString = await rootBundle.loadString(AppConstant.jsonPath);
       List<dynamic> data = jsonDecode(jsonString);
       List<SchoolInfoModel> schoolInfolsit = [];
       for (var element in data) {
